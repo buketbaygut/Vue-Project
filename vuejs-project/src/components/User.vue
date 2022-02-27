9f1209bac25a5ed9767a26600bca9796d17c4149
Buket Ulu <buket.ulu@softtech.com.tr>
2022-02-27 23:52:57 +0300

    Datatable güncellenmiştir.

--- vuejs-project/src/components/User.vue
+++ vuejs-project/src/components/User.vue
@@ -23,7 +23,12 @@
       </v-tab>
 
       <v-tab-item>
-        <v-card-title>
+        <v-card flat>
+          <v-card-text>
+            <v-simple-table>
+    <template v-slot:default>
+      <v-card>
+    <v-card-title>
       <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
@@ -32,58 +37,16 @@
         hide-details
       ></v-text-field>
     </v-card-title>
-        <v-card flat>
-          <v-card-text>
-            <v-simple-table>
-    <template v-slot:default>
-      <thead>
-        <tr>
-          <th class="text-center">
-            Name
-          </th>
-          <th class="text-center">
-            LastName
-          </th>
-          <th class="text-center">
-            City
-          </th>
-          <th class="text-center">
-            Relation
-          </th>
-          <th class="text-center">
-            From Who
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="item in guestList"
-          :key="item.id"
-        >
-          <td>{{ item.firstName }}</td>
-          <td>{{ item.surname }}</td>
-          <td>{{ cities.find(a=>a.id === item.city).name }}</td>
-          <td>{{ item.relation }}</td>
-          <td>{{ item.fromWho }}</td>
-        </tr>
-      </tbody>
+    <v-data-table
+      :headers="headers"
+      :items="guestList"
+      :search="search"
+    ></v-data-table>
+  </v-card>
     </template>
   </v-simple-table>
           </v-card-text>
-          <v-card-title>
-        <v-text-field
-          v-model="search"
-          append-icon="mdi-magnify"
-          label="Search"
-          single-line
-          hide-details
-        ></v-text-field>
-      </v-card-title>
-      <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :search="search"
-      ></v-data-table>
+          
         </v-card>
       </v-tab-item>
       <v-tab-item>
@@ -233,105 +196,23 @@ import cityJson from '../json/city_list.json'
       relation:'',
       fromWhoList : ['Buket','Umut','Both'],
       relationList : ['Family','Friends','Relative'],
-      guestList: [],
       snackbar: false,
       message:'',
       timeout: 2000,
       snackbarColor:'',
       headers: [
           {
-            text: 'Dessert (100g serving)',
+            text: 'Name',
             align: 'start',
             filterable: false,
             value: 'name',
           },
-          { text: 'Calories', value: 'calories' },
-          { text: 'Fat (g)', value: 'fat' },
-          { text: 'Carbs (g)', value: 'carbs' },
-          { text: 'Protein (g)', value: 'protein' },
-          { text: 'Iron (%)', value: 'iron' },
+          { text: 'Surname', value: 'surname' },
+          { text: 'City', value: 'city' },
+          { text: 'Relation', value: 'relation' },
+          { text: 'From Who', value: 'fromWho' },
         ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%',
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%',
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%',
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%',
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%',
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%',
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%',
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%',
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%',
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%',
-          },
+        guestList: [
         ],
     }), 
 
@@ -344,8 +225,13 @@ import cityJson from '../json/city_list.json'
       axios.get("http://localhost:3000/guests")
       .then(function (response) {
             for (var i=0; i<response.data.length; i++) {
-              self.guestList.push(response.data[i])           
-            
+              self.guestList.push({
+                name: response.data[i].firstName,
+                surname: response.data[i].surname,
+                city: self.cities.find(a=>a.id === response.data[i].city).name,
+                relation: response.data[i].relation,
+                fromWho: response.data[i].fromWho,
+              });      
             }
       })                
       .catch(function (error) {
